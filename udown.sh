ec76d2abdb61f13d7bb8c84d51a8fa57ef908254
soIipsist <solipsist14@gmail.com>
2026-05-24 11:15:40 +0300

    udown.sh

---
+++ udown.sh
@@ -0,0 +1,41 @@
+get_json_value() {
+
+  key="$1"
+  shift 1
+  json_file="$1"
+  shift 1
+  default_value="$@"
+
+  if [ ! -f "$json_file" ]; then
+    echo "Error: JSON file does not exist or is not readable."
+    return 1
+  fi
+
+  value=$(jq -r .$key "$json_file")
+
+  if [ "$?" -ne 0 ] || [ "$value" = "null" ]; then
+    value="$default_value"
+  elif [[ "$value" == \[* ]]; then
+    value=$(jq -r .$key[] "$json_file")
+  fi
+
+  echo "$value"
+}
+
+define_downloaders(){
+
+    if ! command -v udown &>/dev/null; then
+        echo "udown is required but not installed."
+        return
+    fi
+
+    for path in *.json; do
+        downloader_type=$(get_json_value "udown.downloader_type" "$path" "$(basename $path)")
+        module=$(get_json_value "udown.module" "$path")
+        downloader_func=$(get_json_value "udown.downloader_func" "$path")
+        downloader_args=$(get_json_value "udown.downloader_args" "$path")
+        
+        udown downloaders add -t "$downloader_type" -m "$module" -f "$downloader_func" -args "$downloader_args" -p "$path"
+    done 
+}
+get_json_value "udown.downloader_args" "quotes.json"
